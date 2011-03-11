c88aed2a68c155bf6ce095b9fab384a7c674e4ca
Sebastian Buesing <buesing.sebastian@googlemail.com>
2011-03-11 16:03:36 +0100

    Added Media Import batch which allows batch assignment of media items to folders on hook_enable()

--- js/media_browser_plus.filter.js
+++ js/media_browser_plus.filter.js
@@ -4,6 +4,9 @@
       // append links
       $('#page').attr('style', 'margin-top:0px; position: absolute; top: 0px; left:0px;');
       $('body').html($('#page'));
+      //
+      if(Drupal.settings.media_browser_plus.autoclose)
+        parent.jQuery.fn.colorbox.close();
     }
-  }
+  };
 })(jQuery);
